dabd53c9023f21d34ed003849fceadcf11e01850
Emre <eeemreaksu@gmail.com>
2025-12-28 01:38:13 +0300

    adım 1

---
+++ script.js
@@ -0,0 +1,27 @@
+const word_el = document.getElementById('word');
+
+function getRandomWord(){
+    const words = ["javascript", "java","python"];
+
+    return words[Math.floor(Math.random()* words.length)];
+}
+
+function displayWord(){
+    const selectedWord = getRandomWord();
+
+
+    word_el.innerHTML= `
+        ${selectedWord.split('').map(letter =>`
+            <div class="letter">
+
+                ${letter}
+
+
+            </div>    
+                `).join('')}
+    
+    
+    `;
+}
+
+displayWord()
